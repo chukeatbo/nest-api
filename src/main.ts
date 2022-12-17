77d8296c36d25d2e2b57dfd1831f6db093c0d513
chukeatbo <chukeat.bo@gmail.com>
2022-12-17 12:05:25 +0700

    Add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 3000;
+  const config = new DocumentBuilder()
+    .setTitle('Nest api')
+    .setDescription('Nest API description')
+    .setVersion('1.0')
+    .addTag('nest-api')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(port, () => {
     console.log("Server is listening on: ", port);
   });
